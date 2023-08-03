bf79529f48059b3a398e761ec6fbd2ac29a8353c
niumengliang <niumengliangrz@howso.cn>
2023-08-03 15:04:20 +0800

    init

---
+++ niu_study_spring_web_check/src/main/java/com/niuml/niu_study_spring_web_check/NiuStudySpringWebCheckApplication.java
@@ -0,0 +1,13 @@
+package com.niuml.niu_study_spring_web_check;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NiuStudySpringWebCheckApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NiuStudySpringWebCheckApplication.class, args);
+    }
+
+}
---
+++ niu_study_spring_web_check/src/test/java/com/niuml/niu_study_spring_web_check/NiuStudySpringWebCheckApplicationTests.java
@@ -0,0 +1,13 @@
+package com.niuml.niu_study_spring_web_check;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NiuStudySpringWebCheckApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
